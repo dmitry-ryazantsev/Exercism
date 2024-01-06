8cb3948e0e8c509af7976de26fa70e8b90b1c71d
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2024-01-06 19:52:17 +0200

    Practise classes by working on a remote controlled car

---
+++ src/Exercism.Tests/RemoteControlCarTests.cs
@@ -0,0 +1,110 @@
+[TestFixture]
+public class RemoteControlCarTests
+{
+    [Test]
+    public void Buy_ReturnsAnObject()
+    {
+        RemoteControlCar car = RemoteControlCar.Buy();
+        Assert.NotNull(car);
+    }
+
+    [Test]
+    public void DistanceDisplay_ZeroMetersForANewCar()
+    {
+        RemoteControlCar car = new RemoteControlCar();
+        var distanceDriven = 0;
+
+        Assert.That(car.DistanceDisplay(), Is.EqualTo($"Driven {distanceDriven} meters"));
+    }
+
+    [Test]
+    public void BatteryDisplay_HundredPercentForANewCar()
+    {
+        RemoteControlCar car = new RemoteControlCar();
+        var batteryCharge = 100;
+
+        Assert.That(car.BatteryDisplay(), Is.EqualTo($"Battery at {batteryCharge}%"));
+    }
+
+    [Test]
+    public void DistanceDisplay_AfterDrivingOnce()
+    {
+        RemoteControlCar car = new RemoteControlCar();
+        var singleDrive = 20;
+
+        car.Drive();
+
+        Assert.That(car.DistanceDisplay(), Is.EqualTo($"Driven {singleDrive} meters"));
+    }
+
+    [Test]
+    public void DistanceDisplay_AfterDrivingMultipleTimes()
+    {
+        RemoteControlCar car = new RemoteControlCar();
+        var distanceDriven = 0;
+
+        for (var i = 0; i < 17; i++)
+        {
+            car.Drive();
+            distanceDriven += 20;
+        }
+
+        Assert.That(car.DistanceDisplay(), Is.EqualTo($"Driven {distanceDriven} meters"));
+    }
+
+    [Test]
+    public void BatteryDisplay_AfterDrivingOnce()
+    {
+        RemoteControlCar car = new RemoteControlCar();
+        var batteryAfterSingleUse = 99;
+
+        car.Drive();
+
+        Assert.That(car.BatteryDisplay(), Is.EqualTo($"Battery at {batteryAfterSingleUse}%"));
+    }
+
+    [Test]
+    public void BatteryDisplay_AfterDrivingMultipleTimes()
+    {
+        RemoteControlCar car = new RemoteControlCar();
+        var batteryCharge = 100;
+
+        for (var i = 0; i < 17; i++)
+        {
+            car.Drive();
+            batteryCharge--;
+        }
+
+        Assert.That(car.BatteryDisplay(), Is.EqualTo($"Battery at {batteryCharge}%"));
+    }
+
+    [Test]
+    public void BatteryDisplay_AfterBatteryEmpty()
+    {
+        RemoteControlCar car = new RemoteControlCar();
+        var batteryCharge = 100;
+
+        for (var i = 0; i < 100; i++)
+        {
+            car.Drive();
+            batteryCharge--;
+        }
+
+        Assert.That(car.BatteryDisplay(), Is.EqualTo($"Battery empty"));
+    }
+
+    [Test]
+    public void DistanceDisplay_AfterBatteryEmpty()
+    {
+        RemoteControlCar car = new RemoteControlCar();
+        var distanceDriven = 0;
+
+        for (var i = 0; i < 100; i++)
+        {
+            car.Drive();
+            distanceDriven += 20;
+        }
+
+        Assert.That(car.DistanceDisplay(), Is.EqualTo($"Driven {distanceDriven} meters"));
+    }
+}
---
+++ src/Exercism/RemoteControlCar.cs
@@ -0,0 +1,20 @@
+public class RemoteControlCar
+{
+    private int _distanceDriven = 0;
+    private int _batteryCharge = 100;
+
+    public static RemoteControlCar Buy() => new RemoteControlCar();
+
+    public string DistanceDisplay() => $"Driven {_distanceDriven} meters";
+
+    public string BatteryDisplay() => _batteryCharge > 0 ? $"Battery at {_batteryCharge}%" : $"Battery empty";
+
+    public void Drive()
+    {
+        if (_batteryCharge > 0)
+        {
+            _distanceDriven += 20;
+            _batteryCharge--;
+        }
+    }
+}
