1cfcc619bf756df0457f0c71bc2806020544e23f
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2024-02-19 19:16:57 +0200

    Proverb: Output the full text of a proverbial rhyme

---
+++ src/Exercism/Proverb.cs
@@ -0,0 +1,21 @@
+namespace Proverb;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        if (subjects.Length == 0)
+            return Array.Empty<string>();
+
+        string[] proverb = new string[subjects.Length];
+
+        for (int i = 0; i < subjects.Length - 1; i++)
+        {
+            proverb[i] = $"For want of a {subjects[i]} the {subjects[i + 1]} was lost.";
+        }
+
+        proverb[^1] = $"And all for the want of a {subjects[0]}.";
+
+        return proverb;
+    }
+}
